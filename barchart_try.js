18ef912d32a61942c247e1323963614f0870012a
Patrick Reynolds <cpreynolds@gmail.com>
2013-06-30 23:39:33 -0400

    Starting a repository for backbone playing.

---
+++ barchart_try.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    "use strict";
+
+    var data = [4, 8, 15, 16, 32, 42];
+
+    var chart = d3.select("body").append("div").attr("class", "chart");
+
+    chart.selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return d * 10 + "px"; })
+      .text(function(d) { return d; });
+
+};
