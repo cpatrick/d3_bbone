4d41132dd4ec7d2793f855611dd62323d976c91f
Patrick Reynolds <cpreynolds@gmail.com>
2013-06-30 23:49:32 -0400

    Moving further through the tutorial.

--- barchart_try.js
+++ barchart_try.js
@@ -1,14 +1,24 @@
 window.onload = function () {
     "use strict";
 
+    // The input data
     var data = [4, 8, 15, 16, 32, 42];
 
-    var chart = d3.select("body").append("div").attr("class", "chart");
+    // Get the chart element.
+    var chart = d3.select("body")
+      .append("div")
+      .attr("class", "chart");
 
+    // Scale the width based on the input data
+    var width = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range(["0px", "420px"]);
+
+    // Link the data with the DOM element
     chart.selectAll("div")
       .data(data)
     .enter().append("div")
-      .style("width", function(d) { return d * 10 + "px"; })
-      .text(function(d) { return d; });
+      .style("width", width)
+      .text(String);
 
 };
